907abccf16576280bce49e793d0cbf0824d14dc0
Andrew McVeigh <andrew.mcveigh@gmail.com>
2016-05-31 14:48:35 -0700

    messing around with a simple maze + Vive

---
+++ Assets/KeyMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/FPS Input")]
+public class KeyMove : MonoBehaviour {
+
+    private CharacterController controller;
+    public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement.y = -9.8f;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        controller.Move(movement);
+	}
+}
---
+++ Assets/Spin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+    public float speed = 3.0f;
+
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, speed, 0);
+        
+	}
+}
